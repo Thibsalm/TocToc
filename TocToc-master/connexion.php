4555ca4738c60a19f481854660cd0fd3f71cfeb7
Thibsalm <44085178+Thibsalm@users.noreply.github.com>
2019-05-16 18:47:42 +0200

    Test

--- TocToc-master/connexion.php
+++ TocToc-master/connexion.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 
-	$bdd = new PDO('mysql:host=localhost;dbname=espace_membre', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=espace_membres', 'root', '');
 	
 	$erreur = array();
 	
